3e27722b0a0651ffd6b9600383e69b74b93a5e72
Vincent Voelz <vvoelz@gmail.com>
2020-12-02 14:41:25 -0500

    Add run loop to make ls wildcards managable

--- length_dist_A8.py
+++ length_dist_A8.py
@@ -1,23 +1,31 @@
 import os, sys, glob, commands
 
-if len(sys.argv) < 3:
-    print "Usage: python length_dist_A8.py PROJDIR ns_per_frame"
+if len(sys.argv) < 4:
+    print "Usage: python2 length_dist_A8.py PROJDIR ns_per_frame nruns"
     sys.exit(1)
 
 projdir = sys.argv[1]
 ns_per_frame = float(sys.argv[2])
+nruns = int(sys.argv[3])
 Continue = True 
 lengths = []
 
 print '#frame\tlength(ns)\tN(frame)'
 frame = 0
-maxframe = 100
+maxframe = 1000
 total_ns = 0.
 while Continue:
-    try:
-        n = int(commands.getoutput('ls -1 %s | wc -l'%os.path.join(projdir, 'RUN*/CLONE*/results%d/frame%d.xtc'%(frame,frame)) ))
-    except:
-        n = 0 
+
+    n = 0
+    # print 'run',
+    for run in range(nruns): 
+        try:
+            n_thisrun = int(commands.getoutput('ls -1 %s | wc -l'%os.path.join(projdir, 'RUN%d/CLONE*/results%d/frame%d.xtc'%(run,frame,frame)) ))
+        except:
+            n_thisrun = 0 
+        n += n_thisrun
+        # print run,  
+    # print
     print '%d\t%d\t%d'%(frame, (frame+1)*ns_per_frame, n)
     total_ns += ns_per_frame*n
     if (n < 1) or frame >= maxframe:
